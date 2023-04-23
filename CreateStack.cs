7c2d905aaadfe6d7f4718a5039d15636e07583c4
venky <venky94.vk@gmail.com>
2023-04-23 23:05:50 +0530

    first commit

---
+++ CreateStack.cs
@@ -0,0 +1,55 @@
+﻿namespace StackQueue
+{
+    public class Stack
+    {
+        private Node top;
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+                this.next = null;
+            }
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+        }
+        public int Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Empty Stack");
+            }
+            int data = top.data;
+            top = top.next;
+            return data;
+        }
+        public int Peek()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return top.data;
+        }
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+      
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+
+        }
+    }
+}
